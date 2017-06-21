088e42d8373693b1f45343b99157b862d3b86c0a
Connor <loligator704@gmail.com>
2017-06-21 16:26:18 -0400

    First Commit
    Ship exists, some code for movement, controls unfinished

---
+++ Assets/_Scripts/Interactables/Controls/RightControls.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Controls : MonoBehaviour {
+	Hand _hand; //not sure if needed
+	ShipController sc;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void HandAttachedUpdate(Hand hand){
+		if (hand.controller.GetTouch(SteamVR_Controller.ButtonMask.Touchpad)) {
+			Vector2 touchAxes = hand.controller.GetAxis();
+			float x = touchAxes.x;
+			float y = touchAxes.y;
+			sc.Roll (x);
+			sc.Pitch (y);
+		}
+	}
+}
---
+++ Assets/_Scripts/ShipController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour {
+	public static ShipController instance;
+	// Use this for initialization
+	Vector3 AngVel;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Roll(float r){ //Around Forward Axis
+		AngVel.z = r;
+	}
+
+	public void Pitch(float p){ //Around Right Axis
+		AngVel.x = p;
+	}
+
+	public void Yaw(float y){ //Around Up axis
+		AngVel.y = y;
+	}
+}
